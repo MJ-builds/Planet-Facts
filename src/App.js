d734625c38c98dec906013e82fe0888852f29950
MJ <112978328+MJ-builds@users.noreply.github.com>
2023-04-22 11:36:35 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import "./App.css";
 
@@ -13,6 +13,7 @@ function App() {
       <Router>
       <Header />
         <Routes>
+        <Route path="/" element={<Navigate to="/planets/earth" />} />
         <Route path="/planets/:activePlanet" element={<PlanetMain />} />
         <Route path="*" element={<ErrorPage />} />
         </Routes>
